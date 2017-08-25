396dab1a3d83aa3130221cc70f3ed7d70bc9691a
s-lucas <sl1331869@gmail.com>
2017-08-25 15:40:55 -0400

    attempting to fix contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "./mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
@@ -37,7 +37,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your message has been sent.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
